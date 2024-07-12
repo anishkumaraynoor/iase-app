0d3906ea92f4156cee2c8efe3f9d27d9ba1181e4
anish <anishpravitha@gmail.com>
2024-07-12 22:32:49 +0530

    completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,12 +9,12 @@ function Header() {
     <>
          <Navbar expand="lg" className="bg-primary">
       <Container>
-        <Navbar.Brand ><Link style={{textDecoration:'none', color:'white'}} to={'/'}>IASE, Thrissur</Link></Navbar.Brand>
+        <Navbar.Brand ><Link style={{textDecoration:'none', color:'white'}} to={'/'}><img src='/iase.jpeg' width={35} height={35}></img>IASE, Thrissur</Link></Navbar.Brand>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link><Link style={{textDecoration:'none', color:'white'}} to={'/data'}>Data</Link></Nav.Link>
             <Nav.Link><Link style={{textDecoration:'none', color:'white'}} to={'/tc'}>TC</Link></Nav.Link>
-            <Nav.Link><Link style={{textDecoration:'none', color:'white'}} to={'/display'}>Display</Link></Nav.Link>
+            <Nav.Link><Link style={{textDecoration:'none', color:'white'}} to={'/display'}>Students</Link></Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import Carousel from 'react-bootstrap/Carousel';
 
 function Home() {
   return (
     <>
-        Home
+        <Carousel>
+      <Carousel.Item>
+        <img src='/image1.jpeg' style={{width:'100%', height:'85vh'}} />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src='/image2.jpeg' style={{width:'100%', height:'85vh'}}  />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src='/image3.jpeg' style={{width:'100%', height:'85vh'}}  />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src='/image4.jpeg' style={{width:'100%', height:'85vh'}}  />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src='/image5.jpeg' style={{width:'100%', height:'85vh'}}  />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src='/image6.jpeg' style={{width:'100%', height:'85vh'}}  />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src='/image7.jpeg' style={{width:'100%', height:'85vh'}}  />
+      </Carousel.Item>
+    </Carousel>
     </>
   )
 }
