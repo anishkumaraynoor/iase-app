d7dafed4335c66669f57ecc900636ed28dc33918
anish <anishpravitha@gmail.com>
2024-07-13 14:33:28 +0530

    completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,9 +7,10 @@ import { Link } from 'react-router-dom';
 function Header() {
   return (
     <>
-         <Navbar expand="lg" className="bg-primary">
+         <Navbar expand="sm" className="bg-primary">
       <Container>
         <Navbar.Brand ><Link style={{textDecoration:'none', color:'white'}} to={'/'}><img src='/iase.jpeg' width={35} height={35}></img>IASE, Thrissur</Link></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />        
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link><Link style={{textDecoration:'none', color:'white'}} to={'/data'}>Data</Link></Nav.Link>
